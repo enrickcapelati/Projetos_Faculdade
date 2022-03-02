aff7ea3b997a575440dc40ca8484cf75eb8d43b8
Enrick Capelati <enrickcapelati@hotmail.com>
2022-03-02 20:55:44 -0300

    Adicionado função de pesquisa por ano e modelo

--- Projeto-1.py
+++ Projeto-1.py
@@ -1,4 +1,5 @@
 opção = 0
+carro=0
 class Carro:
     def __init__(self,marca,modelo,ano,placa):
         self.marca  = marca
@@ -6,24 +7,42 @@ class Carro:
         self.ano    = ano
         self.placa  = placa
     def __str__(self):
-        return  str(self.__class__) + '\n'+ '\n'.join(('{} = {}'.format(item, self.__dict__[item]) for item in self.__dict__))
+        return  ('=-=' * 10) + '\n' +  '\n' + '\n'.join(('{} = {}'.format(item, self.__dict__[item]) for item in self.__dict__)) + '\n' 
+
 lista=[]
 
-def listar():
-    for i in lista:
-        print(i)
-
-def user_input():        
-    marca  = input('Digite a marca:')
-    modelo = input('Digite o modelo:')
-    ano    = input('Digite o ano: ')
-    placa  = input('Digite a placa:') 
-    carro  = Carro(marca,modelo,ano,placa,carro)
+def user_input():     
+    marca  = input('Digite a marca:').lower()
+    modelo = input('Digite o modelo:').lower()
+    ano    = input('Digite o ano: ').lower()
+    placa  = input('Digite a placa:').lower() 
+    global carro
+    carro  = Carro(marca,modelo,ano,placa)
     lista.append(carro)
+ 
 
+def listar():
+    for l in lista:
+        print(l)
 
+def busca_modelo():
+    modelo = input ('Digite o modelo a ser pesquisado: ').lower()
+    for l in lista:
+        if l.modelo == modelo:
+            print(l)
+        else:
+            print('Modelo não encontrado!')
+            continue
 
-
+def ano_busca():
+    ano = input ('Digite o ano a ser pesquisado: ').lower()
+    for l in lista:
+        if l.ano >= ano:
+            print(l)
+        else:
+            print('Não foram encontrados veiculos')
+            continue
+    
 while opção != 5:
     print('''CADASTRO DE VEICULOS
 [1] CADASTRAR NOVO VEICULO
@@ -32,22 +51,18 @@ while opção != 5:
 [4] MOSTRAR VEICULO A PARTIR DO ANO
 [5] SAIR
 ''')
-    opção = int(input('Digite sua opcao: '))
+    opção = int(input('Digite sua opção: '))
+
     if opção == 1:
-        user_input()
-        
+         user_input()
     elif opção == 2:
         listar()
-        
     elif opção == 3:
-        print('opcao 3')
-
+        busca_modelo()
     elif opção == 4:
-        print('opcao 4')
-
+        ano_busca()
     elif opção == 5:
         print('Finalizando...')
-
     else:
         print('Opção inválida tente novamente!')
     print('=-=' * 10)
